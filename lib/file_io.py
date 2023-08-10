1d4f4f30194ee4239b0297845b2db75639830be7
Jimmy Li <jmyli562@gmail.com>
2023-08-10 13:14:42 -0700

    finish lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, mode="w", encoding="utf-8") as log_file:
+        log_file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, mode="a", encoding="utf-8") as log_file:
+        log_file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, encoding="utf-8") as text_file:
+        return text_file.read()
